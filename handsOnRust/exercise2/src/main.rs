c4ca73172f5f70c94a21b0551d4733c8df9afaa0
Semih Yılmaz <semihyilmaz032@gmail.com>
2026-04-07 19:31:50 +0300

    2nd day - 1

--- handsOnRust/exercise2/src/main.rs
+++ handsOnRust/exercise2/src/main.rs
@@ -1,3 +1,50 @@
+struct Wallet {
+    balance: f64,
+}
+
+impl Wallet {
+    fn deposit(&mut self, amount: f64)
+    {
+        if amount > 0f64 {
+            self.balance += amount;
+        }
+    }
+
+    fn withdraw(&mut self, amount: f64) -> bool
+    {
+        if amount < 0f64 {
+            return false;
+        }
+
+        if self.balance > amount {
+            self.balance -= amount;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut wallet = Wallet {balance: 0f64};
+    println!("{}", wallet.balance);
+
+    wallet.deposit(10f64);
+    println!("{}", wallet.balance);
+
+    wallet.withdraw(3f64);
+    println!("{}", wallet.balance);
+
+    wallet.withdraw(10f64);
+    println!("{}", wallet.balance);
+
+    wallet.deposit(10f64);
+    println!("{}", wallet.balance);
+
+    wallet.withdraw(-10f64);
+    println!("{}", wallet.balance);
+
+    wallet.deposit(-10f64);
+    println!("{}", wallet.balance);
 }
