18da8c526e7deceb15f6ec59ee702097a4c3e117
Semih Yılmaz <semihyilmaz032@gmail.com>
2026-04-07 21:06:45 +0300

    2nd day - 3

--- handsOnRust/exercise4/src/main.rs
+++ handsOnRust/exercise4/src/main.rs
@@ -1,3 +1,32 @@
+struct Book {
+    title: String,
+    is_available: bool,
+}
+
+impl Book {
+    fn borrow_book(&mut self) {
+        self.is_available = false;
+    }
+    fn matches_title(&self, search: &str) -> bool {
+        self.title == search
+    }
+    fn return_book(&mut self) {
+        self.is_available = true;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut book = Book { title: String::from("Rust"), is_available: true};
+
+    println!("True {}", book.matches_title(&String::from("Rust")));
+    println!("False {}", book.matches_title(&String::from("asd")));
+    println!("True {}", book.is_available);
+    book.return_book();
+    println!("True {}", book.is_available);
+    book.borrow_book();
+    println!("False {}", book.is_available);
+    book.return_book();
+    println!("True {}", book.is_available);
+    book.borrow_book();
+    println!("False {}", book.is_available);
 }
