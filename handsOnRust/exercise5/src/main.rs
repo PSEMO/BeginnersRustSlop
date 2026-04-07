b1f8324e454b5d0fbc548cc55ef85a7486333b46
Semih Yılmaz <semihyilmaz032@gmail.com>
2026-04-07 21:26:03 +0300

    2nd day - 4

--- handsOnRust/exercise5/src/main.rs
+++ handsOnRust/exercise5/src/main.rs
@@ -1,3 +1,40 @@
+struct Document {
+    content: String,
+}
+
+impl Document {
+    fn first_word(&self) -> &str {
+        let bytes = self.content.as_bytes();
+
+        for (i, &item) in bytes.iter().enumerate() {
+            if item == b' ' {
+                return &self.content[0..i];
+            }
+        }
+
+        &self.content[..]
+    }
+    fn word_count(&self) -> usize {
+        let bytes = self.content.as_bytes();
+
+        let mut word_count = 1;
+        
+        for (i, &item) in bytes.iter().enumerate() {
+            if item == b' ' {
+                word_count += 1;
+            }
+        }
+
+        word_count
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut document = Document { content: String::from("Lorem ipsum.") };
+
+    let word = document.first_word();
+    println!("{}", word);
+
+    let count = document.word_count();
+    println!("{}", count);
 }
