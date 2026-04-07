e080ccb7de590aaedec0c2b1185a64421149c21f
Semih Yılmaz <semihyilmaz032@gmail.com>
2026-04-07 19:45:01 +0300

    2nd day - 2

--- handsOnRust/exercise3/src/main.rs
+++ handsOnRust/exercise3/src/main.rs
@@ -1,3 +1,34 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn scale(&mut self, amount: u32) {
+        self.width *= amount;
+        self.height *= amount;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rectangle = Rectangle {width: 10, height: 20};
+    println!("{}", rectangle.area());
+
+    rectangle.scale(2u32);
+    println!("{}", rectangle.area());
+
+    let mut temp_rectangle = Rectangle {width: 320, height: 3};
+    println!("{}", rectangle.can_hold(&temp_rectangle));
+    temp_rectangle = Rectangle {width: 2, height: 320};
+    println!("{}", rectangle.can_hold(&temp_rectangle));
+    temp_rectangle = Rectangle {width: 3, height: 3};
+    println!("{}", rectangle.can_hold(&temp_rectangle));
 }
